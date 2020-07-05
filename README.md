5751323313a848c87df8f1c10fd14c6cc278b62b
Vaibhav Tulsyan <vstulsyan@gmail.com>
2020-07-05 10:23:53 +0530

    Add option to render the graph

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 - `read-after-write` data dependency graph for variables
 - `statement-level` data dependency graph
 - Inter-procedural data dependency graph
+
+### Visualization
+
+- Install `graphviz` on your machine.
+- Install the python graphviz module using `pip install graphviz`.
+- Pass the relative path from the root directory of this project to the source code. 
---
+++ render.py
@@ -0,0 +1,28 @@
+from sys import argv
+
+from my_visitor import MyVisitor
+from graphviz import Digraph
+
+
+dot = Digraph(comment='DDG')
+
+
+def get_ddg(path):
+    obj = MyVisitor(path)
+    obj.construct_ddg()
+    graphs = obj.get_function_level_ddg()
+    return graphs
+
+
+def render_graph(edges, method_name):
+    dot.edges(edges)
+    dot.save('{0}.dot'.format(method_name))
+    dot.render('{0}.gv'.format(method_name), view=True)
+
+
+# e.g. "snippets/sample_3.py"
+path = argv[1]
+sample_3_graph = get_ddg(path)
+for method_name in sample_3_graph:
+    edges = list(sample_3_graph[method_name])
+    render_graph(edges, method_name)
